d526a43169652602f3e7a392e858159601a8f352
lalitha <lalitha.lisha@gmail.com>
2018-07-11 09:08:15 -0700

    first commit

---
+++ string_operations.rb
@@ -0,0 +1,13 @@
+# Program for String Operations
+
+s="hai"
+r="hello"
+#puts"s+r"
+puts"#{s}#{r}"
+puts"#{s} #{r}"     #for single space
+puts"#{s}      #{r}"
+puts(s+r.squeeze)   #for remove spaces
+
+first,last="sachin,tendulkar"
+puts(first)
+print(first.split(","))
